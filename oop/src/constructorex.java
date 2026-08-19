daa292bcbd5cf766cb4929867ab5fdfabc6b8365
mannoj-cyb-45 <pmanojkumar9972@gmail.com>
2026-08-19 10:32:50 +0530

    contructor conecpt update

--- oop/src/constructorex.java
+++ oop/src/constructorex.java
@@ -1,2 +1,43 @@
-public class constructorex {
+    class Employee{
+        int eid; // instance variable or global variable
+        String ename;
+        void printEmpDet() {
+            System.out.println(eid);
+            System.out.println(ename);
+        }
+    }
+    class Student{
+        int sid;
+        String sname;
+        void printStuDet() {
+            System.out.println(sid);
+            System.out.println(sname);
+        }
+    }
+    class Demo{
+        Demo(){
+            System.out.println("Default Constructor");
+        }
+        Demo(int num){
+            this();// this call the default constructor
+            // this() it executes current class constructor based on parameter
+            System.out.println("Parameterized Constructor: " + num);
+        }
+    }
+    public class constructorex {
+        public static void main(String[] args) {
+            Employee emp = new Employee();
+            emp.eid = 101;
+            emp.ename = "John Doe";
+            emp.printEmpDet();
+
+            Student stu = new Student();
+            stu.sid = 201;
+            stu.sname = "Jane Smith";
+            stu.printStuDet();
+
+            //Demo d2 = new Demo(); //object needed but no need of method call
+            Demo d1 = new Demo(42); // or new Demo();
+            // object needed but no need of method call
+        }
 }
